a13bc4794169f056151af7fdf57efd46cd5f774e
Leonel <leoneljesus5m@gmail.com>
2026-04-03 05:43:26 -0300

    Todo el esquema funcionando correctamente

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(1, 4));
+const otraSum = (a, b) => {
+    return a + b;
+};
+console.log(otraSum("hola ", "como estas"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const sum = (a:number , b:number) : number => {
+    return a + b;
+}
+
+console.log(sum(1,4));
+
+const otraSum = (a:string, b:string) : string => {
+    return a + b;
+}
+
+console.log(otraSum("hola ", "como estas"));
